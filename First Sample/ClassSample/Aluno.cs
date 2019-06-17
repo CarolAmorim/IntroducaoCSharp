0c1554b5855407740cc65c807d907b41d4131c74
Caroline amorim <caroline.lima02@hotmail.com>
2019-06-17 15:26:33 -0300

    Classe em C#

---
+++ First Sample/ClassSample/Aluno.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassSample
+{
+    class Aluno
+    {   // O construtor representa oinstanciamento do objeto
+        //
+        public Aluno()
+        {            
+            Console.WriteLine("Aluno constructor");                       
+        }
+        // Quando não há declaração de visibilidade é considerado privado
+        // Fields
+        private string nome;
+        int idade;
+
+        // Propriedades
+        // sintaxe pública para trabalhar com os fields
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
+        }
+        public int Idade
+        {
+            get
+            {
+                return idade;
+            }
+            set
+            {
+                idade = value;
+            }
+        }
+
+        // Destrutor
+        ~Aluno()
+        {
+            Console.WriteLine("Aluno destrutor");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ First Sample/ClassSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ *  O NameSpace é uma unidade organizacional
+ *  do framework para classes, organizado classes do mesmo tipo
+ */
+namespace ClassSample
+{
+    /* Uma classe representa diferentes estruturas
+     * Instanciamos ela para obter uma unidade em memória dessa classe
+     */ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instancia objeto em aluno
+            //
+            Aluno a1 = new Aluno();
+            a1.Nome = "Carol";
+            a1.Idade = 22;
+            
+            // Espera um click para fechar a janela
+            //
+            Console.ReadKey();
+        }
+    }
+}
